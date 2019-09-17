862465134f5f3fe918670938210117b7afa427d1
John Liu <john.liu@mongodb.com>
2019-09-17 12:24:39 -0400

    initial plumbing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useEffect }  from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Stitch, AnonymousCredential, RemoteMongoClient } from 'mongodb-stitch-browser-sdk';
 
 function App() {
+  useEffect( () => {
+    const stitchClient = Stitch.initializeDefaultAppClient('mongomuseum-mbuqp');
+    console.log("logging in anonymously");
+    stitchClient.auth.loginWithCredential(new AnonymousCredential()).then(user => {
+        console.log(`logged in anonymously as user ${user.id}`)
+    }, err => {
+        console.log(err);
+    });
+    const mongoClient = stitchClient.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas');
+    const db = mongoClient.db('foo');
+    const coll = db.collection('bar')
+    coll.find({}, {limit: 10})
+    .toArray()
+    .then(results => console.log('Results:', results));
+  })
+
   return (
     <div className="App">
       <header className="App-header">
