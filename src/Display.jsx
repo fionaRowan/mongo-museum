566066f6aed2eb09d8a495e659b7f129df227140
John Liu <john.liu@mongodb.com>
2019-09-18 10:46:36 -0400

    hook up to db

--- src/Display.jsx
+++ src/Display.jsx
@@ -1,12 +1,10 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import * as d3 from 'd3'
 import generateRepelGroups from './repelGroups'
 
 const Display = (props) => {
 
-    if (props.store) {
-      console.log(props.store.getState());
-    }
+    const [groups, setGroups] = useState(0);
     const container = useRef(null);
 
     useEffect(() => {
@@ -163,13 +161,19 @@ const Display = (props) => {
                   }
                ],
               }
-              ]
+            ]
           };
 
-          const repelGroups = generateRepelGroups();
-          d3.select(container.current).data([data]).call(repelGroups);
+          if (props.store) {
+            console.log(props.store.getState());
+            setGroups(props.store.getState());
+          }
+          if (groups) {
+            const repelGroups = generateRepelGroups();
+            d3.select(container.current).data([groups[0]]).call(repelGroups);
+          }
         }
-    }, [])
+    }, [groups, props.store])
 
     return (
         <svg
